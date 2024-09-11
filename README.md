86d3e39c49e0ba2f4c51fee67fdfc8a40a52d94d
ahmedbilal <ahmed258bilal@gmail.com>
2024-09-11 22:42:08 +0500

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Personal_Portfolio
+# Personal_Portfolio 
+<h1>Week 1 Task at Developershub corporation</h1>
+Personal Portfolio is the week 1 task wher i create a simple portfoli using HTML,CSS and Js. 
+The website is also responsive for different devices like Mobiles, laptops and PC's etc. 
--- app.js
+++ app.js
@@ -1,34 +1,47 @@
+
+window.addEventListener('hashchange', updateActiveNavLink);
+
+function updateActiveNavLink() {
+    const activePage = window.location.hash;
+    const navLinks = document.querySelectorAll('#nav_bar ul li a');
+    navLinks.forEach(link => {
+        if (link.getAttribute('href') === activePage) {
+            link.classList.add('active'); 
+        } else {
+            link.classList.remove('active'); 
+        }
+    });
+}
+
 function validateForm() {
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
     const message = document.getElementById('message').value;
-   const respond=document.getElementById('respond').innerHTML;
    
     const namePattern = /^[a-zA-Z\s]{1,15}$/; // Only letters, max-length 15
     const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/; // Basic email pattern
     const messagePattern = /^[\s\S]{1,10}$/; // Minimum 10 characters
-
-
+    
     if (!namePattern.test(name)) {
         alert("Please enter a valid name.");
         return false;
     }
 
-  
+
     if (!emailPattern.test(email)) {
         alert("Please enter a valid email address.");
         return false;
     }
 
-   
+
     if (!messagePattern.test(message)) {
         alert("Message must be at least 10 characters long.");
         return false;
     }
-   else{
-    respond.style.display="flex";
-     return true; // Allow form submission
-     
-   }
-   
+    else {
+       alert("Message has been delivered scuuessfully");
+        return true; // Allow form submission
+        
+    }
+
 }
